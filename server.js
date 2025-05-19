5cd85b859a00eef94bbaa0ce0eb050f9ff0796fe
sunny55g <140750855+sunny55g@users.noreply.github.com>
2025-05-19 21:34:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -75,12 +75,12 @@ app.listen(port, () => {
 
 
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log("✅ MongoDB Connected"))
-.catch(err => console.error("❌ MongoDB Error:", err));
+// mongoose.connect(process.env.MONGO_URI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// })
+// .then(() => console.log("✅ MongoDB Connected"))
+// .catch(err => console.error("❌ MongoDB Error:", err));
 
 
 
