a672864317dc9b77a2b8631e390b2924245933f7
sunny55g <140750855+sunny55g@users.noreply.github.com>
2025-05-20 18:27:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,9 @@ const app = express();
 const port = process.env.PORT || 3010;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'https://harsh-suthar-portfolio.netlify.app/'
+}));
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
